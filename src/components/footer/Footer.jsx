4bac46b3a2552f205a081ffe8ea762b874f94551
Jahongirov01 <153820313+Jahongirov01@users.noreply.github.com>
2024-05-13 22:01:44 +0500

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container">
+        <p>All rights reserved.</p>
+        <a href="https://github.com/">Jahongirov Ibrohimjon</a>
+      </div>
+    </div>
+  )
+}
